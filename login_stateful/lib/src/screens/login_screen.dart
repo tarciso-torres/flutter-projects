e18462ac99980b818c3c089c2170d381a1c1740a
Tarciso Torres <tarcisoftorres@gmail.com>
2019-09-20 22:00:36 -0300

    Create the login screen

---
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(){
+    
+  }
+}
