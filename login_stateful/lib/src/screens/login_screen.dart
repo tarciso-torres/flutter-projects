dff12880472c274d0ad6fb44f8802ba36727229e
Tarciso Torres <tarcisoftorres@gmail.com>
2019-09-22 13:09:55 -0300

    Create a global key

--- login_stateful/lib/src/screens/login_screen.dart
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -8,10 +8,14 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+
+  final formKey = GlobalKey<FormState>();
+
   Widget build(context){
     return Container(
       margin: EdgeInsets.all(20),
       child: Form(
+        key: formKey,
         child: Column(
           children: <Widget>[
             emailField(),
